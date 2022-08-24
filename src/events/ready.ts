7ac19cbb0370db1c70003d8b08207c1e54bea8f7
Dominik Spitzli <dominik@spitzli.dev>
2022-08-24 20:36:26 +0200

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,21 @@
-import type Client from '../classes/Client';
+import type Client from '../classes/Client.js';
+import type Event from '../types/Event.js';
 
-export default {
+export const event: Event = {
 	name: 'ready',
 	once: true,
-	execute(client: Client) {
-		console.log(`Ready! Logged in as ${client.user?.tag || 'undefined'}`);
+	async execute(client: Client) {
+		new Promise(async (res, rej) => {
+			try {
+				client.log.info(
+					`Ready, logged in as ${client.user?.tag || 'undefined'}!`,
+				);
+				res(0);
+			} catch (e) {
+				rej(1);
+			}
+		});
 	},
 };
+
+export default event;
