1fa49bc66ff85995e2eee5548b05230a6dd5d6da
Dominik Spitzli <dominik@spitzli.dev>
2022-08-24 20:36:37 +0200

    Update Event.ts

--- src/types/Event.ts
+++ src/types/Event.ts
@@ -1,5 +1,8 @@
+import type { ClientEvents } from 'discord.js';
+import type Client from '../classes/Client.js';
+
 export default interface Event {
-	name: string;
+	name: keyof ClientEvents;
 	once?: boolean;
-	execute(...args: any[]): Promise<void>;
+	execute(client: Client, ...args: any[]): Promise<void>;
 }
