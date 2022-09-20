82250ed7656549d0ee74f1cc7a1763c284e78f12
HarakuneSan <71511883+HarakuneSan@users.noreply.github.com>
2022-09-20 19:32:51 +0200

    Update ping to match commit #51ed119

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,7 @@ import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 import type Command from '../types/Command.js';
 
 export const command: Command = {
-	data: new SlashCommandBuilder().setName('ping').setDescription('pong'),
+	options: new SlashCommandBuilder().setName('ping').setDescription('pong'),
 	execute(interaction: CommandInteraction): Promise<0 | 1> {
 		return new Promise(async (res) => {
 			await interaction.reply("OWO I'm pong!");
